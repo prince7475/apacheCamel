b954dd96650107bcbde35a95d3f415a993ed6a7c
Prince Donkor <prince7475>
2019-02-04 19:14:42 -0600

    restarted

--- src/main/java/com/pluralsight/orderfulfillment/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.orderfulfillment.customer;
-
-import org.springframework.data.jpa.repository.*;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
-
-}
---
+++ src/main/java/com/princedonkor/camelapplication/CamelApplication.java
@@ -0,0 +1,14 @@
+package com.princedonkor.camelapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+
+}
+
